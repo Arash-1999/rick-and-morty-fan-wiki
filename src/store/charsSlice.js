aec33abc382e0e3aef06a6369fe3057b291d05b4
spry_ps <shafieearash2000@yahoo.com>
2021-08-26 09:16:24 -0400

    reducer changed to use api pageination

--- src/store/charsSlice.js
+++ src/store/charsSlice.js
@@ -3,43 +3,57 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   loading: true,
   data: [],
-  page: 1
+  page: "https://rickandmortyapi.com/api/character"
 };
 
+// character slice
 const charSlice = createSlice({
   name: "chars",
   initialState,
   reducers: {
     addChars: (state, action) => {
-      state.page++;
-      state.data = [...state.data, ...action.payload];
+      state.page = action.payload.next;
+      state.data = [...state.data, ...action.payload.data];
     },
     setChars: (state, action) => {
-      state.page = 2;
-      state.data = action.payload;
+      state.page = action.payload.next;
+      state.data = action.payload.data;
     }
   }
 });
 
+// character slice reducers
+// this will export as default
 export default charSlice.reducer;
 
+// character slice actions
 export const { addChars, setChars } = charSlice.actions;
 
+// load characters
 export const getChars = () => async (dispatch, getState) => {
-  let url = "https://rickandmortyapi.com/api/character",
-    page = getState().chars.page;
-  let data = await fetch(page ? url + `?page=${page}` : url);
+  // get new page url
+  let url = getState().chars.page;
+  // check validation of new page url
+  if(url === null) return;
+  // fetch data from api
+  let data = await fetch(url);
   data = await data.json();
 
-  dispatch(addChars(data.results));
+  // add new data to store
+  dispatch(addChars({data: data.results, next: data.info.next}));
 };
 
+// thunk for getting characters by given query
 export const getCharsByQuery = (query) => async (dispatch) => {
+  // default endpoint
   let url = "https://rickandmortyapi.com/api/character/?name=";
+  // fetch data from api
   let data = await fetch(url + query);
   data = await data.json();
 
-  dispatch(setChars(data.results));
+  // add new data to store
+  dispatch(setChars({data: data.results, next: data.info.next}));
 };
 
+// selecting characters
 export const selectChars = (state) => state.chars;
