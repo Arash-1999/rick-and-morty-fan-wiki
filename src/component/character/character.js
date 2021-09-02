abf6aaad82a3f6f0031b8c3c783a2daf5a7dafd6
spry_ps <shafieearash2000@yahoo.com>
2021-09-02 18:32:03 +0430

    getting single character from api added

--- src/component/character/character.js
+++ src/component/character/character.js
@@ -1,13 +1,26 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { selectSingleChar } from "./../../store/singleCharSlice.js";
+import {
+  getSingleChar,
+  selectSingleChar
+} from "./../../store/singleCharSlice.js";
 
 import Loading from "./loading.js";
 import Button from "./../btn.js";
 
+/*** *** *** *** *** *** *** ***/
+// styles
+/*** *** *** *** *** *** *** ***/
+
+/*** *** *** *** *** *** *** ***/
+// character component
 const Character = ({ match, history }) => {
-  const { loading } = useSelector(selectSingleChar);
+  const dispatch = useDispatch();
+  const { loading, char } = useSelector(selectSingleChar);
+
+  // eslint-disable-next-line
+  useEffect(() => dispatch(getSingleChar(match.params.id)), []);
 
   // back to home event listener
   const goHome = ()  => {
@@ -16,10 +29,23 @@ const Character = ({ match, history }) => {
 
   return (
     <main>
-      {loading ? (<Loading />) : (<p>data loaded</p>)}
+      {loading ? (<Loading />) : (
+        <section>
+          <img src={char.image} alt={char.name} />
+          <ul>
+            <li>Name: {char.name ? char.name : "Unkown"}</li>
+            <li>Status: {char.status ? char.status : "Unkown"}</li>
+            <li>Species: {char.species ? char.species : "Unkown"}</li>
+            <li>Type: {char.type ? char.type : "Unkown"}</li>
+            <li>Gender: {char.gender ? char.gender : "Unkown"}</li>
+            <li>Origin: {char.origin.name ? char.origin.name : "Unkown"}</li>
+          </ul>
+        </section>
+      )}
       <Button onClick={goHome}>Back to home</Button>
     </main>
   );
 };
+/*** *** *** *** *** *** *** ***/
 
 export default Character;
--- src/store/singleCharSlice.js
+++ src/store/singleCharSlice.js
@@ -9,9 +9,33 @@ const singleCharSlice = createSlice({
   name: "singleChar",
   initialState,
   reducers: {
+    setChar: (state, action) => {
+      state.char = action.payload;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    }
   }
 });
 
+// reducers
 export default singleCharSlice.reducer;
 
+// actions
+export const { setChar, setLoading } = singleCharSlice.actions;
+
+// thunk for getting single character from api
+export const getSingleChar = (id) => async (dispatch) => {
+  // set loading status
+  dispatch(setLoading(true));
+  // default endpoint
+  let url = "https://rickandmortyapi.com/api/character/";
+  let data = await fetch(url + id);
+  data = await data.json();
+
+  dispatch(setChar(data));
+  dispatch(setLoading(false));
+};
+
+// selectors
 export const selectSingleChar = (state) => state.singleChar;
